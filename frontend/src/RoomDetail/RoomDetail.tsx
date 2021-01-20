21073786babe4380c22d22e649f38d34e2e65f88
kenoga <nogaken1107@gmail.com>
2021-01-21 08:45:35 +0900

    use function component for room detail

--- frontend/src/RoomDetail/RoomDetail.tsx
+++ frontend/src/RoomDetail/RoomDetail.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import "./RoomDetail.css";
 import Calendar, {
   CalendarTileProperties,
@@ -14,94 +14,90 @@ type DetailParam = {
   dateString: string;
 };
 
-
 type TimeSlotId = number;
 type TimeSlotInfo = {
-  name: string,
-status: boolean
+  name: string;
+  status: boolean;
 };
-type TimeSlotInfos = Map<TimeSlotId, TimeSlotInfo>
+type TimeSlotInfos = Map<TimeSlotId, TimeSlotInfo>;
 
-type State = {
-  selectedTimeSlot: TimeSlotId | null,
-  dateString: string,
-  roomStatus: TimeSlotInfos 
-}
+
 
 function pickEmpty(roomStatus: TimeSlotInfos): TimeSlotId | null {
   for (let key of Array.from(roomStatus.keys())) {
-    if (roomStatus.get(key)?.status) return key
+    if (roomStatus.get(key)?.status) return key;
   }
-  return null
+  return null;
 }
 
 const testRoomStatus: TimeSlotInfos = new Map([
-  [1, { name: "12 - 17PM", status: false}],
-  [2, { name: "18 - 23PM", status: true}],
-  [3, { name: "24 - 05AM", status: true}],
-])
+  [1, { name: "12 - 17PM", status: false }],
+  [2, { name: "18 - 23PM", status: true }],
+  [3, { name: "24 - 05AM", status: true }],
+]);
 
-class RoomDetail extends React.Component<RouteComponentProps<DetailParam>, State> {
-  constructor(props: RouteComponentProps<DetailParam>) {
-    super(props);
-    this.state = { selectedTimeSlot: null, dateString: props.match.params.dateString, roomStatus: new Map() };
-    
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.componentDidMount = this.componentDidMount.bind(this);
-  }
+function RoomDetail(props: RouteComponentProps<DetailParam>) {
+  let [selectedTimeSlot, setTimeSlot] = useState<TimeSlotId>(1);
+  let [dateString, setDateString] = useState(props.match.params.dateString);
+  let [roomStatus, setRoomStatus] = useState(new Map());
 
-  componentDidMount() {
-    this.setState({ roomStatus: testRoomStatus })
-  }
+  useEffect(() => {
+    setRoomStatus(testRoomStatus);
+  }, [dateString]);
 
-  handleSubmit() {
-    console.log("submit!");
-    console.log(this.state.selectedTimeSlot)
-    console.log(this.state.dateString)
-    fetch("https://www.google.com/").then((response) => { console.log(response)});
-  }
+  return (
+    <div className="roomDetail">
+      <p>ABCマンション中目黒の詳細</p>
+      <h3>{dateString}</h3>
 
-  handleChange(e: ChangeEvent<HTMLInputElement>): void {
-    console.log(e.target.value)
-    this.setState({selectedTimeSlot: parseInt(e.target.value)})
-  }
+      <div onChange={ (e) => handleChange(e, setTimeSlot) } className="radioButtons">
+        {Array.from(roomStatus.keys()).map((timeSlotId) => {
+          return returnRadioButton(timeSlotId, roomStatus, selectedTimeSlot);
+        })}
+      </div>
+      <div className="text-center">
+        <Button variant="primary" onClick={ (e) => handleSubmit(selectedTimeSlot, dateString) } size="lg">
+          予約する
+        </Button>
+      </div>
+    </div>
+  );
+}
 
-  returnRadioButton(timeSlotId: number): JSX.Element {
-    return (
+function handleSubmit(selectedTimeSlot: TimeSlotId, dateString: string) {
+  console.log("submit!");
+  console.log(selectedTimeSlot);
+  console.log(dateString);
+  fetch("https://www.google.com/").then((response) => {
+    console.log(response);
+  });
+}
+
+function handleChange(
+  e: any,
+  setTimeSlot: (timeSlot: TimeSlotId) => void
+): void {
+  console.log(e.target.value);
+  setTimeSlot(parseInt(e.target.value));
+}
+
+function returnRadioButton(
+  timeSlotId: number,
+  roomStatus: TimeSlotInfos,
+  selectedTimeSlot: TimeSlotId
+): JSX.Element {
+  return (
     <Form.Check
       type="radio"
-      label={this.state.roomStatus.get(timeSlotId)?.name}
-      name={this.state.roomStatus.get(timeSlotId)?.name}
+      label={roomStatus.get(timeSlotId)?.name}
+      name={roomStatus.get(timeSlotId)?.name}
       value={timeSlotId}
-      checked={this.state.selectedTimeSlot == timeSlotId}
-      disabled={!this.state.roomStatus.get(timeSlotId)?.status}
+      checked={selectedTimeSlot == timeSlotId}
+      disabled={!roomStatus.get(timeSlotId)?.status}
       className="radioButton"
-     />
-    )
-  }
-
-  render(): JSX.Element {
-    return (
-      <div className="roomDetail">
-        <p>ABCマンション中目黒の詳細</p>
-        <h3>{this.state.dateString}</h3>
-        
-        <div onChange={this.handleChange} className="radioButtons">
-          {Array.from(
-            this.state.roomStatus.keys()
-            ).map((timeSlotId) => { return this.returnRadioButton(timeSlotId) })
-          }
-        </div>
-        <div className="text-center">
-          <Button variant="primary" onClick={this.handleSubmit} size="lg">
-            予約する
-          </Button>
-        </div>
-
-      </div>
-    );
-  }
+    />
+  );
 }
+
 
 export default RoomDetail;
