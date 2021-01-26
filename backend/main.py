770060d9f9e7f3baaeb1dc96eeebb949a2bc78c1
kenoga <nogaken1107@gmail.com>
2021-01-27 08:25:51 +0900

    apply lint

--- backend/main.py
+++ backend/main.py
@@ -1,26 +1,27 @@
+from .database import SessionLocal
+from .util import unauthorized, hash_str, randomstr
+from .service import auth
+from . import service
+from .import crud
 import logging
-import datetime, calendar
+import datetime
+import calendar
 from typing import Optional
-import enum
 
 from fastapi import FastAPI, Cookie, Response, Depends
 from fastapi.logger import logger
 
 from sqlalchemy.orm import Session
 
-from pydantic import BaseModel
-
-from .schema import *
-from .import crud
-from . import model
-from . import service
-from .service import auth
-from .util import unauthorized, hash_str, randomstr, now 
-from .database import SessionLocal
+from .schema import SuccessResponse, LoginRequest, success, GatewayRequest, \
+    GatewayAction, GatewayStatusResponse, \
+    ReservationResponse, ReserveRequest, MyReservationsResponse, \
+    DateCalendarResponse, MonthCalendarResponse
 
 app = FastAPI()
 
 logger = logging.getLogger(__name__)
+
 
 def get_db():
     db = SessionLocal()
@@ -38,17 +39,21 @@ async def root():
 """
 User related API
 """
+
+
 @app.post("/api/user", response_model=SuccessResponse)
 def post_user(param: LoginRequest,
               db: Session = Depends(get_db)) -> SuccessResponse:
     crud.create_user(db, param.email, hash_str(param.password), "")
-    return success() 
+    return success()
+
 
 @app.post("/api/login", response_model=SuccessResponse)
 def login(param: LoginRequest,
           response: Response,
           db: Session = Depends(get_db)) -> SuccessResponse:
-    user = crud.fetch_user_with_sessions(db, param.email, hash_str(param.password))
+    user = crud.fetch_user_with_sessions(
+        db, param.email, hash_str(param.password))
     if user is None:
         unauthorized()
     if user.sessions:
@@ -60,8 +65,9 @@ def login(param: LoginRequest,
         token = randomstr(64)
         crud.create_session(db, user.id, token)
         logger.info("new session token: " + token)
-        response.set_cookie("session_key", token) 
-    return success() 
+        response.set_cookie("session_key", token)
+    return success()
+
 
 @app.post("/api/logout", response_model=SuccessResponse)
 def logout(response: Response,
@@ -75,12 +81,19 @@ def logout(response: Response,
 """
 Calendar API
 """
-@app.get("/api/calendar/{playground_id}/{date}", response_model=DateCalendarResponse)
+
+
+@app.get("/api/calendar/{playground_id}/{date}",
+         response_model=DateCalendarResponse)
 def get_date_reservation(playground_id: int,
                          date: datetime.date,
                          db: Session = Depends(get_db)):
     result = service.get_date_calendar(db, playground_id, date)
-    return DateCalendarResponse(playground_id=playground_id, date=date, reserved=result)
+    return DateCalendarResponse(
+        playground_id=playground_id,
+        date=date,
+        reserved=result)
+
 
 @app.get("/api/calendar/{playground_id}", response_model=MonthCalendarResponse)
 def get_month_reservation(playground_id: int,
@@ -96,18 +109,25 @@ def get_month_reservation(playground_id: int,
 """
 Reservation API
 """
-@app.get("/api/user/{user_id}/reservations", response_model=MyReservationsResponse)
+
+
+@app.get("/api/user/{user_id}/reservations",
+         response_model=MyReservationsResponse)
 def user_reservations(user_id: int,
                       session_key: Optional[str] = Cookie(None),
                       db: Session = Depends(get_db)):
     # TODO: sorting
     try:
         user = auth(db, session_key)
-        return MyReservationsResponse(reservations=service.get_user_reservations(db, user))
+        return MyReservationsResponse(
+            reservations=service.get_user_reservations(
+                db, user))
     finally:
         db.close()
 
-@app.get("/api/user/{user_id}/reservations/active", response_model=ReservationResponse)
+
+@app.get("/api/user/{user_id}/reservations/active",
+         response_model=ReservationResponse)
 def user_active_reservation(user_id: int,
                             session_key: Optional[str] = Cookie(None),
                             db: Session = Depends(get_db)):
@@ -116,45 +136,59 @@ def user_active_reservation(user_id: int,
         unauthorized()
     return service.get_user_active_reservation(db, user)
 
-    
+
 @app.post("/api/reserve")
 def reserve(request: ReserveRequest,
             session_key: Optional[str] = Cookie(None),
             db: Session = Depends(get_db)):
     user = auth(db, session_key)
-    start_at, end_at = service.resolve_time_range(request.date, request.time_range)
-    service.check_reservation_duplicate(db, request.playground_id, request.date, request.time_range)
-    
-    crud.create_reservation(db, user.id, request.playground_id, request.date, request.time_range, start_at, end_at)
-    return success()
+    start_at, end_at = service.resolve_time_range(
+        request.date, request.time_range)
+    service.check_reservation_duplicate(
+        db, request.playground_id, request.date, request.time_range)
 
+    crud.create_reservation(
+        db,
+        user.id,
+        request.playground_id,
+        request.date,
+        request.time_range,
+        start_at,
+        end_at)
+    return success()
 
 
 """
 Key API
 """
 # TODO: 鍵操作APIの定義
+
+
 @app.put("/api/gateway/{action}", response_model=SuccessResponse)
 def put_gateway(request: GatewayRequest,
                 action: GatewayAction,
                 session_key: Optional[str] = Cookie(None),
                 db: Session = Depends(get_db)):
     user = auth(db, session_key)
-    gateway_session = service.validate_gateway_session(db, request.gateway_session_key, user)
+    gateway_session = service.validate_gateway_session(
+        db, request.gateway_session_key, user)
 
     if action == GatewayAction.LOCK:
         service.lock_gateway(db, gateway_session)
     else:
         service.unlock_gateway(db, gateway_session)
-    
+
     return success()
+
 
 @app.get("/api/gateway", response_model=SuccessResponse)
 def get_gateway_status(request: GatewayRequest,
                        session_key: Optional[str] = Cookie(None),
                        db: Session = Depends(get_db)):
     user = auth(db, session_key)
-    gateway_session = service.validate_gateway_session(db, request.gateway_session_key, user)
-    
-    return GatewayStatusResponse(gateway_id=gateway_session.id, status=get_gateway_status(gateway_session))
-    
+    gateway_session = service.validate_gateway_session(
+        db, request.gateway_session_key, user)
+
+    return GatewayStatusResponse(
+        gateway_id=gateway_session.id,
+        status=get_gateway_status(gateway_session))
