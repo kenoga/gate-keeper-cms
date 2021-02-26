e27d921ed29d662eb03b1ae60738ec471f5338f1
kenoga <nogaken1107@gmail.com>
2021-02-26 15:23:27 +0900

    fix1

--- upload_images.sh
+++ upload_images.sh
@@ -7,7 +7,7 @@ cd ../
 docker build -t gatekeeper -f Dockerfile .
 docker build -t gatekeeper-nginx -f NginxDockerfile .
 
-docker tag gatekeeper-nginx:latest 411596790806.dkr.ecr.ap-northeast-1.amazonaws.com/gatekeeper:latest
+docker tag gatekeeper:latest 411596790806.dkr.ecr.ap-northeast-1.amazonaws.com/gatekeeper:latest
 docker tag gatekeeper-nginx:latest 411596790806.dkr.ecr.ap-northeast-1.amazonaws.com/gatekeeper-nginx:latest
 
 aws ecr get-login-password --region ap-northeast-1 --profile gatekeeper | docker login --username AWS --password-stdin 411596790806.dkr.ecr.ap-northeast-1.amazonaws.com
